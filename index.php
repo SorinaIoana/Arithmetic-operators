98c80491aeb5b001fd4e93f009c4df7d5421dbb9
SorinaIoana <118562406+SorinaIoana@users.noreply.github.com>
2023-05-09 09:59:17 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$x = 10;
+$y = 5;
+
+echo "$x + $y este egal cu: ". $x + $y .".<br>";
+echo "$x - $y este egal cu: ". $x - $y .".<br>";
+echo "$x * $y este egal cu: ". $x * $y .".<br>";
+echo "$x / $y este egal cu: ". $x / $y .".<br>";
+
+?>
